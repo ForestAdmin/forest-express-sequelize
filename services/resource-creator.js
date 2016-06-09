6d26a6dae4ddaa25b46bf18e64c5ebe5a9bc98c2
Arnaud Besnier <arnaudibesnier@gmail.com>
2016-06-09 18:13:23 -0400

    Set HasMany Associations on create and update

--- services/resource-creator.js
+++ services/resource-creator.js
@@ -1,10 +1,26 @@
 'use strict';
+var _ = require('lodash');
+var P = require('bluebird');
 var ResourceGetter = require('./resource-getter');
 
 function ResourceCreator(model, params) {
 
   this.perform = function () {
     return model.create(params)
+      .then(function(record) {
+        var promises = [];
+
+        if (model.associations) {
+          _.forOwn(model.associations, function(association, name) {
+            if (['HasOne', 'HasMany', 'BelongsToMany']
+              .indexOf(association.associationType) > -1) {
+              promises.push(record[`set${_.capitalize(name)}`](params[name]));
+            }
+          });
+        }
+
+        return P.all(promises).thenReturn(record);
+      })
       .then(function (record) {
         return new ResourceGetter(model, { recordId: record.id });
       });
--- services/resource-updater.js
+++ services/resource-updater.js
@@ -1,4 +1,6 @@
 'use strict';
+var _ = require('lodash');
+var P = require('bluebird');
 var ResourceGetter = require('./resource-getter');
 
 function ResourceUpdater(model, params) {
@@ -8,6 +10,23 @@ function ResourceUpdater(model, params) {
       .update(params, {
         where: { id: params.id },
         individualHooks: true
+      })
+      .then(function () {
+        return model.findById(params.id);
+      })
+      .then(function(record) {
+        var promises = [];
+
+        if (model.associations) {
+          _.forOwn(model.associations, function(association, name) {
+            if (['HasOne', 'HasMany', 'BelongsToMany']
+              .indexOf(association.associationType) > -1) {
+              promises.push(record[`set${_.capitalize(name)}`](params[name]));
+            }
+          });
+        }
+
+        return P.all(promises);
       })
       .then(function () {
         return new ResourceGetter(model, { recordId: params.id }).perform();
