d78ab3027d6538615399a519f03b89b44e15ef06
Arnaud Besnier <arnaudibesnier@gmail.com>
2016-07-19 08:32:03 -0400

    Users want to create pie charts that are grouped by a FK

--- services/pie-stat-getter.js
+++ services/pie-stat-getter.js
@@ -1,9 +1,21 @@
 'use strict';
+var _ = require('lodash');
 var P = require('bluebird');
 var OperatorValueParser = require('./operator-value-parser');
 
 // jshint sub: true
 function PieStatGetter(model, params, opts) {
+  var associatedField;
+  var associatedValues;
+
+  function detectGroupByAssociationField() {
+    _.values(model.associations).forEach(function (association) {
+      if (params['group_by_field'] === association.target.name) {
+        associatedField = association.foreignKey;
+      }
+    });
+  }
+
   function getAggregate() {
     return params.aggregate.toLowerCase();
   }
@@ -25,10 +37,48 @@ function PieStatGetter(model, params, opts) {
     return filters;
   }
 
+  function getGroupBy() {
+    return associatedField || params['group_by_field'];
+  }
+
+  function retrieveAssociatedValuesIfAny (records) {
+    if (!associatedField) { return records; }
+
+    var associatedIds = _.map(records, getGroupBy());
+    return model.associations[params['group_by_field']].target
+      .findAll({ where: { id: { $in: associatedIds }}})
+      .then(function (values) {
+        associatedValues = _.map(values, 'dataValues');
+      })
+      .thenReturn(records);
+  }
+
+  function formatResults (records) {
+    return P.map(records, function (record) {
+      record = record.toJSON();
+      var key;
+
+      if (associatedValues) {
+        key = _.find(associatedValues, function (value) {
+          return value.id === record[getGroupBy()];
+        });
+      } else {
+        key = String(record[getGroupBy()]);
+      }
+
+      return {
+        key: key,
+        value: record.value
+      };
+    });
+  }
+
   this.perform = function () {
+    detectGroupByAssociationField();
+
     return model.findAll({
       attributes: [
-        params['group_by_field'],
+        getGroupBy(),
         [
           opts.sequelize.fn(getAggregate(),
           opts.sequelize.col(getAggregateField())),
@@ -36,19 +86,11 @@ function PieStatGetter(model, params, opts) {
         ]
       ],
       where: getFilters(),
-      group: [params['group_by_field']],
+      group: [getGroupBy()],
       order: 'value DESC'
     })
-    .then(function (records) {
-      return P.map(records, function (record) {
-        record = record.toJSON();
-
-        return {
-          key: String(record[params['group_by_field']]),
-          value: record.value
-        };
-      });
-    })
+    .then(retrieveAssociatedValuesIfAny)
+    .then(formatResults)
     .then(function (records) {
       return { value: records };
     });
