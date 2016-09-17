ab411a1719ee7db1f0f2d9377e96fad4fe915190
Sandro Munda <sandro@munda.me>
2016-09-17 17:55:14 +0200

    Fix the search when an association field comes from an integration

--- services/resources-getter.js
+++ services/resources-getter.js
@@ -6,73 +6,6 @@ var Interface = require('forest-express');
 function ResourcesGetter(model, opts, params) {
   var schema = Interface.Schemas.schemas[model.name];
 
-  function getIncludes() {
-    var includes = [];
-
-    _.values(model.associations).forEach(function (association) {
-      if (['HasOne', 'BelongsTo'].indexOf(association.associationType) > -1) {
-        includes.push({
-          model: association.target,
-          as: association.associationAccessor
-        });
-      }
-    });
-
-    return includes;
-  }
-
-  function getOrder() {
-    if (params.sort) {
-      var order = 'ASC';
-
-      if (params.sort[0] === '-') {
-        params.sort = params.sort.substring(1);
-        order = 'DESC';
-      }
-
-
-      return [[params.sort, order]];
-    }
-
-    return [];
-  }
-
-  function getRecordsFromResult(result) {
-    return result.map(function (r) {
-      r = r.toJSON();
-
-      // Ensure the Serializer set the relationship links on has many
-      // relationships by setting them to an empty array.
-      _.values(model.associations).forEach(function (association) {
-        if (['HasMany', 'BelongsToMany'].indexOf(association.associationType) > -1) {
-          r[association.associationAccessor] = [];
-        }
-      });
-
-      return r;
-    });
-  }
-
-  function getRecords() {
-    return model
-      .findAll({
-        include: getIncludes(),
-        limit: getLimit(),
-        offset: getSkip(),
-        where: getWhere(),
-        order: getOrder()
-      })
-      .then((result) => getRecordsFromResult(result));
-  }
-
-  function getCount() {
-    return model
-      .count({
-        include: getIncludes(),
-        where: getWhere()
-      });
-  }
-
   function handleSearchParam() {
     var where = {};
     var or = [];
@@ -80,6 +13,9 @@ function ResourcesGetter(model, opts, params) {
     _.each(schema.fields, function (field) {
       // Ignore Smart field.
       if (field.isVirtual) { return; }
+
+      // Ignore integration field.
+      if (field.integration) { return; }
 
       var q = {};
 
@@ -181,6 +117,56 @@ function ResourcesGetter(model, opts, params) {
     } else {
       return 0;
     }
+  }
+
+  function getIncludes() {
+    var includes = [];
+
+    _.values(model.associations).forEach(function (association) {
+      if (['HasOne', 'BelongsTo'].indexOf(association.associationType) > -1) {
+        includes.push({
+          model: association.target,
+          as: association.associationAccessor
+        });
+      }
+    });
+
+    return includes;
+  }
+
+  function getOrder() {
+    if (params.sort) {
+      var order = 'ASC';
+
+      if (params.sort[0] === '-') {
+        params.sort = params.sort.substring(1);
+        order = 'DESC';
+      }
+
+
+      return [[params.sort, order]];
+    }
+
+    return [];
+  }
+
+  function getRecords() {
+    return model
+      .findAll({
+        include: getIncludes(),
+        limit: getLimit(),
+        offset: getSkip(),
+        where: getWhere(),
+        order: getOrder()
+      });
+  }
+
+  function getCount() {
+    return model
+      .count({
+        include: getIncludes(),
+        where: getWhere()
+      });
   }
 
   this.perform = function () {
