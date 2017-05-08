c22b7d330119430e95f79fd806b1a8edb7e0f376
Arnaud Besnier <arnaudibesnier@gmail.com>
2017-05-08 19:47:15 +0200

    [*] HasOne Associations - Users can now update records with hasOne associations

--- services/belongs-to-updater.js
+++ services/belongs-to-updater.js
@@ -6,8 +6,32 @@ function BelongsToUpdater(model, association, opts, params, data) {
     return model
       .findById(params.recordId)
       .then(function (record) {
-        return record['set' + _.capitalize(params.associationName)](
-          data.data ? data.data.id : null);
+        // WORKAROUND: Make the hasOne associations update work while waiting
+        //             for the Sequelize 4 release with the fix of the following
+        //             issue: https://github.com/sequelize/sequelize/issues/6069
+        // TODO: Once Sequelize 4 is mainstream, use the following code instead:
+        //       return record['set' + _.capitalize(params.associationName)](
+        //         data.data ? data.data.id : null);
+        var isHasOne = false;
+        var modelAssociation;
+
+        _.each(model.associations, function (association) {
+          if (association.associationAccessor === params.associationName) {
+            isHasOne = association.associationType === 'HasOne';
+            modelAssociation = association.target;
+          }
+        });
+
+        if (isHasOne && data.data) {
+          return modelAssociation
+            .findById(data.data.id)
+            .then(function (recordAssociated) {
+              record['set' + _.capitalize(params.associationName)](recordAssociated);
+            });
+        } else {
+          return record['set' + _.capitalize(params.associationName)](
+            data.data ? data.data.id : null);
+        }
       });
   };
 }
