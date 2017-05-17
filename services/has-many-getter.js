af3cf9f3668be772911d8c7b9beae416334db0ea
Arnaud Besnier <arnaudibesnier@gmail.com>
2017-05-17 14:18:09 +0200

    [*] HasMany ListViews - Fix the display of hasMany records for a model having a primaryKey that is not an "id" column and that has the related column hidden in the hasMany list view (#95)

--- services/has-many-getter.js
+++ services/has-many-getter.js
@@ -1,16 +1,14 @@
 'use strict';
 var _ = require('lodash');
 var P = require('bluebird');
-var Interface = require('forest-express');
 
 function HasManyGetter(model, association, opts, params) {
-  var schema = Interface.Schemas.schemas[association.name];
 
   function getFieldNamesRequested() {
     if (!params.fields || !params.fields[association.name]) { return null; }
     // NOTICE: Force the primaryKey retrieval to store the records properly in
     //         the client.
-    var primaryKeyArray = [_.keys(model.primaryKeys)[0]];
+    var primaryKeyArray = [_.keys(association.primaryKeys)[0]];
 
     return _.union(primaryKeyArray, params.fields[association.name].split(','));
   }
@@ -43,18 +41,6 @@ function HasManyGetter(model, association, opts, params) {
       .then(function (records) {
         return records.length;
       });
-  }
-
-  function getSelect() {
-    var fieldNamesRequested = getFieldNamesRequested();
-    if (!fieldNamesRequested) { return null; }
-
-    var fieldsSchema = _.select(schema.fields, function (field) {
-      return !field.reference && !field.isVirtual;
-    });
-    var fieldNamesSchema = _.map(fieldsSchema, 'field');
-
-    return _.intersection(fieldNamesSchema, fieldNamesRequested);
   }
 
   function hasPagination() {
@@ -99,7 +85,6 @@ function HasManyGetter(model, association, opts, params) {
       .then(function (record) {
         return record['get' + _.capitalize(params.associationName)]({
           scope: false,
-          attributes: getSelect(),
           offset: getSkip(),
           limit: getLimit(),
           include: getIncludes(),
