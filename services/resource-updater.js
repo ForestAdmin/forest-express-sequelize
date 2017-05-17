a58811779900c3b6cb3851a6f6b2028d15884ea1
Lucas Scariot <lucas@forestadmin.com>
2017-05-17 15:34:28 +0200

    [+] Ressource updater

--- services/resource-updater.js
+++ services/resource-updater.js
@@ -1,17 +1,35 @@
 'use strict';
 var ResourceGetter = require('./resource-getter');
 var Interface = require('forest-express');
+var _ = require('lodash');
 
 function ResourceUpdater(model, params) {
   var schema = Interface.Schemas.schemas[model.name];
 
   this.perform = function () {
     var where = {};
-    where[schema.idField] = params[schema.idField];
+
+    if (schema.isCompositePrimary) {
+      _.keys(model.primaryKeys).forEach(function (key) {
+        where[key] = params[key];
+      });
+    } else {
+      where[schema.idField] = params[schema.idField];
+    }
 
     return model
       .update(params, { where: where, individualHooks: true })
       .then(function () {
+        if (schema.isCompositePrimary) {
+          params.forestCompositePrimary = '';
+          _.keys(model.primaryKeys).forEach(function (key, index) {
+            var glue = '';
+            if (index > 0) { glue = '-'; }
+            params.forestCompositePrimary = params.forestCompositePrimary +
+              glue +
+              params[key];
+          });
+        }
         return new ResourceGetter(model, {
           recordId: params[schema.idField]
         }).perform();
