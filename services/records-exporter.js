15eb49d787d287b4dbd6d1740da6c927260ba4e0
Arnaud Besnier <arnaudibesnier@gmail.com>
2017-08-10 16:27:45 +0200

    Make a first batch requester for export

--- services/records-exporter.js
+++ services/records-exporter.js
@@ -1,25 +1,36 @@
 'use strict';
-// var _ = require('lodash');
-var P = require('bluebird');
-// var OperatorValueParser = require('./operator-value-parser');
-// var Interface = require('forest-express');
-// var CompositeKeysManager = require('./composite-keys-manager');
-// var QueryBuilder = require('./query-builder');
-// var SearchBuilder = require('./search-builder');
+var ResourcesGetter = require('./resources-getter');
 
-function RecordsExporter() {
+var BATCH_INITIAL_PAGE = 1;
+var BATCH_SIZE = 1000;
+
+function RecordsExporter(model, options, params) {
+  // TODO: do not hardcode the primary key here.
+  params.sort = 'id';
+  params.page = { size: BATCH_SIZE };
+
+  function retrieveBatch(dataSender, pageNumber) {
+    params.page.number = pageNumber;
+    return new ResourcesGetter(model, options, params)
+      .perform()
+      .then(function (results) {
+        var records = results[1];
+
+        return dataSender(records)
+          .then(function () {
+            // if (records.length === BATCH_SIZE) {
+            if (records.length === BATCH_SIZE && pageNumber < 10) {
+              console.log('page: ', pageNumber);
+              return retrieveBatch(dataSender, pageNumber + 1);
+            } else {
+              return;
+            }
+          });
+      });
+  }
+
   this.perform = function (dataSender) {
-    dataSender('id, name, address\n');
-
-    return new P(function (resolve) {
-      setTimeout(function() {
-        dataSender('1, sandy, 11 rue des vergers\n');
-      }, 3000);
-      setTimeout(function() {
-        dataSender('3, toto, 13 rue des vergers\n');
-        resolve();
-      }, 6000);
-    });
+    return retrieveBatch(dataSender, BATCH_INITIAL_PAGE);
   };
 }
 
