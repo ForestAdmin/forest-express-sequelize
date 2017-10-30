0c1f4a554957f64993cff52b4ea0332d54843a0f
Arnaud Besnier <arnaudibesnier@gmail.com>
2017-10-30 16:07:11 +0100

    [*] HasMany Dissociation - Catch SQL constraints errors on record dissociation

--- services/has-many-dissociator.js
+++ services/has-many-dissociator.js
@@ -1,5 +1,6 @@
 'use strict';
 var _ = require('lodash');
+var ErrorHTTP422 = require('./errors').ErrorHTTP422;
 
 function HasManyAssociator(model, association, opts, params, data) {
   this.perform = function () {
@@ -11,6 +12,9 @@ function HasManyAssociator(model, association, opts, params, data) {
         });
         return record['remove' + _.capitalize(params.associationName)](
           associatedIds);
+      })
+      .catch(function (error) {
+        throw new ErrorHTTP422(error.message);
       });
   };
 }
