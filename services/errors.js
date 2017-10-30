60d23833438a5c0ee3b9ad4398842a216ebc6100
Arnaud Besnier <arnaudibesnier@gmail.com>
2017-10-30 16:10:32 +0100

    Indentation

--- services/errors.js
+++ services/errors.js
@@ -1,9 +1,9 @@
 'use strict';
 function ErrorHTTP422(message) {
-    this.name = 'ErrorHTTP422';
-    this.message = message || 'Unprocessable Entity';
-    this.status = 422;
-    this.stack = (new Error()).stack;
+  this.name = 'ErrorHTTP422';
+  this.message = message || 'Unprocessable Entity';
+  this.status = 422;
+  this.stack = (new Error()).stack;
 }
 ErrorHTTP422.prototype = new Error();
 
