5a6d3aa033428653680d8389f0f82da7090ed603
Arnaud Besnier <arnaudibesnier@gmail.com>
2018-01-22 15:43:23 +0100

    Defensive programming on the query absence

--- services/query-stat-getter.js
+++ services/query-stat-getter.js
@@ -1,10 +1,18 @@
 'use strict';
+var Interface = require('forest-express');
 
 function QueryStatGetter(params, opts) {
   var QUERY_OPTIONS_SELECT = { type: opts.sequelize.QueryTypes.SELECT };
 
   this.perform = function () {
     var rawQuery = params.query;
+
+    if (!rawQuery) {
+      Interface.logger
+        .error('Cannot execute an empty SQL query using Live Query feature.');
+      return [];
+    }
+
     if (params.record_id) {
       rawQuery = rawQuery.replace('?', params.record_id);
     }
