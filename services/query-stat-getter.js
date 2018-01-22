d888d5bfbc15e520a7ef9334f7e70de1b1d9475a
Arnaud Besnier <arnaudibesnier@gmail.com>
2018-01-22 15:38:19 +0100

    Clean the Query Stat Getter

--- services/query-stat-getter.js
+++ services/query-stat-getter.js
@@ -1,19 +1,17 @@
 'use strict';
-const _ = require('lodash');
 
 function QueryStatGetter(params, opts) {
+  var QUERY_OPTIONS_SELECT = { type: opts.sequelize.QueryTypes.SELECT };
 
   this.perform = function () {
+    var rawQuery = params.query;
     if (params.record_id) {
-      params.query = params.query.replace('?', params.record_id);
+      rawQuery = rawQuery.replace('?', params.record_id);
     }
 
-    return opts.connections[0].query(params.query, {
-      type: opts.sequelize.QueryTypes.SELECT
-    })
-    .then(function (result) {
-      return result;
-    });
+    // WARNING: Choosing the first connection might generate issues if the model
+    //          does not belongs to this database.
+    return opts.connections[0].query(rawQuery, QUERY_OPTIONS_SELECT);
   };
 }
 
