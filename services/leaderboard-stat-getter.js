373db58d72e0b97348548ae79e9000b9dda10c7a
David Routhieau <rootio@protonmail.com>
2018-06-04 16:34:28 +0200

    Fix aggregate

--- services/leaderboard-stat-getter.js
+++ services/leaderboard-stat-getter.js
@@ -1,37 +1,45 @@
 'use strict';
+var Interface = require('forest-express');
 var BaseStatGetter = require('./base-stat-getter');
 
-function LeaderboardStatGetter(model, params, opts) {
+function LeaderboardStatGetter(model, relationshipModel, params, opts) {
   BaseStatGetter.call(this, model, params, opts);
 
-  var collectionField = params.collection_field;
-  var relationship = params.relationship;
+  var collectionField = params['collection_field'];
   var aggregate = params.aggregate.toUpperCase();
+  var aggregateField = params['aggregate_field'];
   var limit = params.limit;
 
-  var key = model.name.toLowerCase() + '.' + collectionField;
-  var groupBy = '"' + key + '"';
-  var aggregateKey = opts.sequelize.Utils.singularize(relationship);
-  var aggregateAlias = aggregateKey + '.id';
+  var schema = Interface.Schemas.schemas[relationshipModel.name];
+
+  var groupBy = model.name.toLowerCase() + '.' + collectionField;
+
+  function getAggregateField() {
+    // NOTICE: As MySQL cannot support COUNT(table_name.*) syntax, fieldName
+    //         cannot be '*'.
+    var fieldName = aggregateField || schema.primaryKeys[0] ||
+      schema.fields[0].field;
+    return schema.name + '.' + fieldName;
+  }
 
   this.perform = function () {
-    return model.associations[relationship].target
+    return relationshipModel
       .unscoped()
       .findAll({
-        attributes: [[opts.sequelize.fn(aggregate, aggregateAlias), 'value']],
+        attributes: [[opts.sequelize.fn(aggregate, opts.sequelize.col(getAggregateField())), 'value']],
         include: [{
           model: model,
           attributes: [collectionField],
         }],
-        group: [opts.sequelize.literal(groupBy)],
+        group: groupBy,
         order: [[opts.sequelize.literal('value'), 'DESC']],
         limit,
         raw: true
       })
       .then(function(records) {
         return records.map(function (data) {
-          data.key = data[key];
-          delete data[key];
+          data.key = data[groupBy];
+          delete data[groupBy];
           return data;
         });
       })
