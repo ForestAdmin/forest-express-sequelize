67cf9d84a8105cd0178c60364b2603ce3b06618d
Arnaud Besnier <arnaudibesnier@gmail.com>
2018-06-29 11:21:50 +0200

    [*] Smart Fields - Always pass a Sequelize instance as the first parameter of the Smart Field value getter functions (#200)

--- services/resource-getter.js
+++ services/resource-getter.js
@@ -1,5 +1,4 @@
 'use strict';
-var _ = require('lodash');
 var createError = require('http-errors');
 var Interface = require('forest-express');
 var CompositeKeysManager = require('./composite-keys-manager');
@@ -16,18 +15,6 @@ function ResourceGetter(model, params) {
           throw createError(404, 'The ' + model.name + ' #' + params.recordId +
             ' does not exist.');
         }
-
-        // NOTICE: Do not use "toJSON" method to prevent issues on models that
-        //         override this method.
-        record = record.get({ plain: true });
-
-        // Ensure the Serializer set the relationship links on has many
-        // relationships by setting them to an empty array.
-        _.values(model.associations).forEach(function (association) {
-          if (['HasMany', 'BelongsToMany'].indexOf(association.associationType) > -1) {
-            record[association.associationAccessor] = [];
-          }
-        });
 
         if (schema.isCompositePrimary) {
           record.forestCompositePrimary = new CompositeKeysManager(model,
