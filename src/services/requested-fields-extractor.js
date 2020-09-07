0a7e2b39e8a6884a3b96b06155dba70742681847
Guillaume Gautreau <guillaumeg@forestadmin.com>
2020-09-07 15:35:46 +0200

    fix: :bug: fix retrieved fields from the database when a smart field is used as a reference field
    Linked to CU-7yqxa0

--- src/services/requested-fields-extractor.js
+++ src/services/requested-fields-extractor.js
@@ -1,28 +1,62 @@
+/**
+ * @typedef {{
+ *  fields: Array<{
+ *    field: string;
+ *    isVirtual: boolean;
+ *  }>
+ * }} Schema
+ */
 
 /**
- * @param {Record<string, string>} fields
- * @param {*} modelOrAssociation
+ * @param {string} associationName
+ * @param {string[]} requestedFields
+ * @param {Schema} schema
  * @returns {string[]}
  */
-function extractRequestedFields(fields, modelOrAssociation) {
-  if (!fields || !fields[modelOrAssociation.name]) { return null; }
+function extractRequestedFieldsForAssociation(associationName, requestedFields, schema) {
+  const referencesSmartFields = requestedFields.some(
+    (fieldName) => schema.fields.some((field) => field.field === fieldName && field.isVirtual),
+  );
+
+  // TODO: have a way of specifying which actual field is used for the computation of a smart field
+  //       and use that info to only retrieve fields that are needed
+  if (referencesSmartFields) {
+    return [`${associationName}`];
+  }
+
+  return requestedFields.map((fieldName) => `${associationName}.${fieldName}`);
+}
+
+/**
+ * @param {Record<string, string>} requestedFields
+ * @param {*} modelOrAssociation
+ * @param {{
+ *  [collection: string]: Schema
+ * }} schemas
+ * @returns {string[]}
+ */
+function extractRequestedFields(requestedFields, modelOrAssociation, schemas) {
+  if (!requestedFields || !requestedFields[modelOrAssociation.name]) { return null; }
 
   // NOTICE: Force the primaryKey retrieval to store the records properly in
   //         the client.
   const primaryKeyArray = [Object.keys(modelOrAssociation.primaryKeys)[0]];
 
   const allAssociationFields = Object.keys(modelOrAssociation.associations)
-    // NOTICE: Remove fields for which attributes are not explicitely set
+    // NOTICE: Remove fields for which attributes are not explicitly set
     //         in the requested fields
-    .filter((associationName) => fields[associationName])
-    .map((associationName) => {
-      const associationFields = fields[associationName].split(',');
-      return associationFields.map((fieldName) => `${associationName}.${fieldName}`);
-    })
+    .filter((associationName) => requestedFields[associationName])
+    .map((associationName) => extractRequestedFieldsForAssociation(
+      associationName,
+      requestedFields && requestedFields[associationName]
+        ? requestedFields[associationName].split(',')
+        : [],
+      schemas[modelOrAssociation.associations[associationName].target.name],
+    ))
     .flat();
 
-  const modelFields = fields[modelOrAssociation.name].split(',')
-    .filter((fieldName) => !fields[fieldName]);
+  const modelFields = requestedFields[modelOrAssociation.name].split(',')
+    .filter((fieldName) => !requestedFields[fieldName]);
 
   return Array.from(new Set([
     ...primaryKeyArray,
@@ -30,6 +64,5 @@ function extractRequestedFields(fields, modelOrAssociation) {
     ...allAssociationFields,
   ]));
 }
-
 
 module.exports = extractRequestedFields;
