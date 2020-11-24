227d0027e42bf9894699090ef8146e33fa908402
Guillaume Gautreau <guillaumeg@forestadmin.com>
2020-11-24 16:39:00 +0100

    fix: always return primary keys of associated records

--- src/services/query-builder.js
+++ src/services/query-builder.js
@@ -3,7 +3,77 @@ import { Schemas } from 'forest-express';
 import Orm from '../utils/orm';
 import Database from '../utils/database';
 
+const HAS_ONE = 'HasOne';
+const BELONGS_TO = 'BelongsTo';
+
 const { getReferenceField } = require('../utils/query');
+
+/**
+ * @param {string[]} values
+ * @returns {string[]}
+ */
+function uniqueValues(values) {
+  return Array.from(new Set(values));
+}
+
+/**
+ * @param {string} key
+ * @param {import('sequelize').Association} association
+ * @returns {string}
+ */
+function getTargetFieldName(key, association) {
+  // Defensive programming
+  if (key && association.target.tableAttributes[key]) {
+    return association.target.tableAttributes[key].fieldName;
+  }
+
+  return undefined;
+}
+
+/**
+ * @param {import('sequelize').BelongsTo&import('sequelize').BelongsToOptions} association
+ * @returns {string[]}
+ */
+function getMandatoryFieldsForBelongsTo(association) {
+  return [
+    getTargetFieldName(association.targetKey, association),
+  ].filter(Boolean);
+}
+
+/**
+ * @param {import('sequelize').HasOne & import('sequelize').HasOneOptions} association
+ * @returns {string[]}
+ */
+function getMandatoryFieldsForHasOne(association) {
+  return [
+    getTargetFieldName(association.sourceKey, association),
+  ].filter(Boolean);
+}
+
+/**
+ * @param {import('sequelize').HasOne|import('sequelize').BelongsTo} association
+ * @returns {string[]}
+ */
+function getMandatoryFields(association) {
+  const primaryKeys = association.target.primaryKeyAttributes
+    .map((attribute) => getTargetFieldName(attribute, association));
+
+  if (association.associationType === BELONGS_TO) {
+    return uniqueValues([
+      ...primaryKeys,
+      ...getMandatoryFieldsForBelongsTo(association),
+    ]);
+  }
+
+  if (association.associationType === HAS_ONE) {
+    return uniqueValues([
+      ...primaryKeys,
+      ...getMandatoryFieldsForHasOne(association),
+    ]);
+  }
+
+  return [];
+}
 
 function QueryBuilder(model, opts, params) {
   const schema = Schemas.schemas[model.name];
@@ -30,7 +100,7 @@ function QueryBuilder(model, opts, params) {
     const includes = [];
 
     Object.values(modelForIncludes.associations)
-      .filter((association) => ['HasOne', 'BelongsTo'].includes(association.associationType))
+      .filter((association) => [HAS_ONE, BELONGS_TO].includes(association.associationType))
       .forEach((association) => {
         const targetFields = Object.values(association.target.tableAttributes)
           .map((attribute) => attribute.fieldName);
@@ -45,10 +115,10 @@ function QueryBuilder(model, opts, params) {
         || explicitAttributes.length) {
           // NOTICE: For performance reasons, we only request the keys
           //         as they're the only needed fields for the interface
-          const uniqueExplicitAttributes = Array.from(new Set([
-            association.targetKey,
+          const uniqueExplicitAttributes = uniqueValues([
+            ...getMandatoryFields(association),
             ...explicitAttributes,
-          ].filter(Boolean)));
+          ].filter(Boolean));
 
           const attributes = explicitAttributes.length
             ? uniqueExplicitAttributes
