f3ecb26f37cc0edf4921211b5f3148369c2895ad
Vincent Molinié <vincent.molinie@epita.fr>
2021-01-22 16:55:25 +0100

    test: test all operators

--- test/utils/operators.test.js
+++ test/utils/operators.test.js
@@ -9,6 +9,17 @@ describe('utils > operators', () => {
       const Op = new Operators();
       expect(Op.AND).toStrictEqual('$and');
       expect(Op.CONTAINS).toStrictEqual('$contains');
+      expect(Op.EQ).toStrictEqual('$eq');
+      expect(Op.GT).toStrictEqual('$gt');
+      expect(Op.GTE).toStrictEqual('$gte');
+      expect(Op.IN).toStrictEqual('$in');
+      expect(Op.LIKE).toStrictEqual('$like');
+      expect(Op.LT).toStrictEqual('$lt');
+      expect(Op.LTE).toStrictEqual('$lte');
+      expect(Op.NE).toStrictEqual('$ne');
+      expect(Op.NOT).toStrictEqual('$not');
+      expect(Op.NOT_LIKE).toStrictEqual('$notLike');
+      expect(Op.OR).toStrictEqual('$or');
     });
   });
 
@@ -17,8 +28,19 @@ describe('utils > operators', () => {
       expect.assertions(2);
 
       const Op = new Operators({ sequelize });
-      expect(Op.AND).toStrictEqual(sequelize.Op.AND);
-      expect(Op.CONTAINS).toStrictEqual(sequelize.Op.CONTAINS);
+      expect(Op.AND).toStrictEqual(sequelize.Op.and);
+      expect(Op.CONTAINS).toStrictEqual(sequelize.Op.contains);
+      expect(Op.EQ).toStrictEqual(sequelize.Op.eq);
+      expect(Op.GT).toStrictEqual(sequelize.Op.gt);
+      expect(Op.GTE).toStrictEqual(sequelize.Op.gte);
+      expect(Op.IN).toStrictEqual(sequelize.Op.in);
+      expect(Op.LIKE).toStrictEqual(sequelize.Op.like);
+      expect(Op.LT).toStrictEqual(sequelize.Op.lt);
+      expect(Op.LTE).toStrictEqual(sequelize.Op.lte);
+      expect(Op.NE).toStrictEqual(sequelize.Op.ne);
+      expect(Op.NOT).toStrictEqual(sequelize.Op.not);
+      expect(Op.NOT_LIKE).toStrictEqual(sequelize.Op.notLike);
+      expect(Op.OR).toStrictEqual(sequelize.Op.or);
     });
   });
 });
