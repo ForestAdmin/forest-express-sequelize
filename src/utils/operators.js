a80332d932c795b35685735d24d0a5d1cf0aa31e
Vincent Molinié <vincent.molinie@epita.fr>
2021-03-05 15:21:01 +0100

    chore: fix last CC issue

--- src/utils/operators.js
+++ src/utils/operators.js
@@ -5,36 +5,44 @@ class Operators {
     return !!(options && options.Sequelize && options.Sequelize.Op);
   }
 
+  _setupNewSequelizeOp(options) {
+    const { Op } = options.Sequelize;
+    this.AND = Op.and;
+    this.CONTAINS = Op.contains;
+    this.EQ = Op.eq;
+    this.GT = Op.gt;
+    this.GTE = Op.gte;
+    this.IN = Op.in;
+    this.LIKE = Op.like;
+    this.LT = Op.lt;
+    this.LTE = Op.lte;
+    this.NE = Op.ne;
+    this.NOT = Op.not;
+    this.NOT_LIKE = Op.notLike;
+    this.OR = Op.or;
+  }
+
+  _setupOldSequelizeOp() {
+    this.AND = '$and';
+    this.CONTAINS = '$contains';
+    this.EQ = '$eq';
+    this.GT = '$gt';
+    this.GTE = '$gte';
+    this.IN = '$in';
+    this.LIKE = '$like';
+    this.LT = '$lt';
+    this.LTE = '$lte';
+    this.NE = '$ne';
+    this.NOT = '$not';
+    this.NOT_LIKE = '$notLike';
+    this.OR = '$or';
+  }
+
   constructor(options) {
     if (Operators._isNewSequelizeOp(options)) {
-      const { Op } = options.Sequelize;
-      this.AND = Op.and;
-      this.CONTAINS = Op.contains;
-      this.EQ = Op.eq;
-      this.GT = Op.gt;
-      this.GTE = Op.gte;
-      this.IN = Op.in;
-      this.LIKE = Op.like;
-      this.LT = Op.lt;
-      this.LTE = Op.lte;
-      this.NE = Op.ne;
-      this.NOT = Op.not;
-      this.NOT_LIKE = Op.notLike;
-      this.OR = Op.or;
+      this._setupNewSequelizeOp(options);
     } else {
-      this.AND = '$and';
-      this.CONTAINS = '$contains';
-      this.EQ = '$eq';
-      this.GT = '$gt';
-      this.GTE = '$gte';
-      this.IN = '$in';
-      this.LIKE = '$like';
-      this.LT = '$lt';
-      this.LTE = '$lte';
-      this.NE = '$ne';
-      this.NOT = '$not';
-      this.NOT_LIKE = '$notLike';
-      this.OR = '$or';
+      this._setupOldSequelizeOp();
     }
 
     Operators._instance = this;
