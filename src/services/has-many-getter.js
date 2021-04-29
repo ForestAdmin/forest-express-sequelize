ea8d14c0e46d7766605656dbbc2f4993170b0630
Romain Gilliotte <romaing@forestadmin.com>
2021-04-29 15:16:51 +0200

    fix: pass user to parent class in has-many-getter

--- src/services/has-many-getter.js
+++ src/services/has-many-getter.js
@@ -2,8 +2,8 @@ import CompositeKeysManager from './composite-keys-manager';
 import ResourcesGetter from './resources-getter';
 
 class HasManyGetter extends ResourcesGetter {
-  constructor(model, association, lianaOptions, params) {
-    super(association, lianaOptions, params);
+  constructor(model, association, lianaOptions, params, user) {
+    super(association, lianaOptions, params, user);
 
     this._parentModel = model.unscoped();
   }
