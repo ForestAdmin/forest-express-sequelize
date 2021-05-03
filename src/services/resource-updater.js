f613c01025f71837c7f4dc16d88d39e386031a88
Romain Gilliotte <romaing@forestadmin.com>
2021-05-03 11:07:28 +0200

    refactor: rewrite as class and use query-options generator on resource-updater

--- src/services/resource-updater.js
+++ src/services/resource-updater.js
@@ -1,32 +1,32 @@
-const { ErrorHTTP422 } = require('./errors');
-const ResourceGetter = require('./resource-getter');
-const PrimaryKeysManager = require('./primary-keys-manager');
-const ResourceFinder = require('./resource-finder');
+import { ErrorHTTP422 } from './errors';
+import QueryOptions from './query-options';
+import ResourceGetter from './resource-getter';
 
-function ResourceUpdater(model, params, newRecord) {
-  this.perform = () => {
-    const compositeKeysManager = new PrimaryKeysManager(model);
+class ResourceUpdater {
+  constructor(model, params, newRecord) {
+    this._model = model.unscoped();
+    this._params = params;
+    this._newRecord = newRecord;
+  }
 
-    return new ResourceFinder(model, params)
-      .perform()
-      .then((record) => {
-        if (record) {
-          Object.assign(record, newRecord);
+  async perform() {
+    const queryOptions = new QueryOptions(this._model);
+    await queryOptions.filterByIds([this._params.recordId]);
 
-          return record.validate()
-            .catch((error) => { throw new ErrorHTTP422(error.message); })
-            .then(() => record.save());
-        }
-        return null;
-      })
-      .then(() => {
-        compositeKeysManager.annotateRecords([newRecord]);
+    const record = await this._model.findOne(queryOptions.sequelizeOptions);
+    if (record) {
+      Object.assign(record, this._newRecord);
 
-        return new ResourceGetter(model, {
-          recordId: params.recordId,
-        }).perform();
-      });
-  };
+      try {
+        await record.validate();
+      } catch (error) {
+        throw new ErrorHTTP422(error.message);
+      }
+      record.save();
+    }
+
+    return new ResourceGetter(this._model, { recordId: this._params.recordId }).perform();
+  }
 }
 
 module.exports = ResourceUpdater;
