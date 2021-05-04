42ab7a25cf0c9d376c3f220c94fb25afc5aa087d
Valentin Lamatte <valentinl@forestadmin.com>
2021-05-04 11:37:10 +0200

    feat: apply scopes on leaderboard charts

--- src/services/leaderboard-stat-getter.js
+++ src/services/leaderboard-stat-getter.js
@@ -1,47 +1,55 @@
 import _ from 'lodash';
-import { Schemas } from 'forest-express';
+import { Schemas, scopeManager } from 'forest-express';
 import Orm from '../utils/orm';
 import { InvalidParameterError } from './errors';
+import QueryOptions from './query-options';
 
 function getAggregateField({
-  aggregateField, schemaRelationship, modelRelationship,
+  aggregateField, schemaRelationship, parentModel,
 }) {
   // NOTICE: As MySQL cannot support COUNT(table_name.*) syntax, fieldName cannot be '*'.
   const fieldName = aggregateField
     || schemaRelationship.primaryKeys[0]
     || schemaRelationship.fields[0].field;
-  return `${modelRelationship.name}.${Orm.getColumnName(schemaRelationship, fieldName)}`;
+  return `${parentModel.name}.${Orm.getColumnName(schemaRelationship, fieldName)}`;
+}
+
+async function getSequelizeOptionsForModel(model, user, timezone) {
+  const queryOptions = new QueryOptions(model);
+  const scopeFilters = await scopeManager.getScopeForUser(user, model.name, true);
+  await queryOptions.filterByConditionTree(scopeFilters, timezone);
+  return queryOptions.sequelizeOptions;
 }
 
 /**
- * @param {import('sequelize').Model} model
- * @param {import('sequelize').Model} modelRelationship
+ * @param {import('sequelize').Model} childModel
+ * @param {import('sequelize').Model} parentModel
  * @param {{
  *  label_field: string;
  *  aggregate: string;
  *  aggregate_field: string;
  * }} params
  */
-function LeaderboardStatGetter(model, modelRelationship, params) {
+function LeaderboardStatGetter(childModel, parentModel, params, user) {
   const labelField = params.label_field;
   const aggregate = params.aggregate.toUpperCase();
   const { limit } = params;
-  const schema = Schemas.schemas[model.name];
-  const schemaRelationship = Schemas.schemas[modelRelationship.name];
+  const schema = Schemas.schemas[childModel.name];
+  const schemaRelationship = Schemas.schemas[parentModel.name];
   let associationAs = schema.name;
   const associationFound = _.find(
-    modelRelationship.associations,
-    (association) => association.target.name === model.name,
+    parentModel.associations,
+    (association) => association.target.name === childModel.name,
   );
 
   const aggregateField = getAggregateField({
     aggregateField: params.aggregate_field,
     schemaRelationship,
-    modelRelationship,
+    parentModel,
   });
 
   if (!associationFound) {
-    throw new InvalidParameterError(`Association ${model.name} not found`);
+    throw new InvalidParameterError(`Association ${childModel.name} not found`);
   }
 
   if (associationFound.as) {
@@ -52,26 +60,32 @@ function LeaderboardStatGetter(model, modelRelationship, params) {
   const groupBy = `${associationAs}.${labelColumn}`;
 
   this.perform = async () => {
-    const records = await modelRelationship
-      .unscoped()
-      .findAll({
-        attributes: [
-          [model.sequelize.col(groupBy), 'key'],
-          [model.sequelize.fn(aggregate, model.sequelize.col(aggregateField)), 'value'],
-        ],
-        includeIgnoreAttributes: false,
-        include: [{
-          model,
-          attributes: [labelField],
-          as: associationAs,
-          required: true,
-        }],
-        subQuery: false,
-        group: groupBy,
-        order: [[model.sequelize.literal('value'), 'DESC']],
-        limit,
-        raw: true,
-      });
+    const { timezone } = params;
+
+    const parentSequelizeOptions = await getSequelizeOptionsForModel(parentModel, user, timezone);
+    const childSequelizeOptions = await getSequelizeOptionsForModel(childModel, user, timezone);
+
+    const records = await parentModel.findAll({
+      attributes: [
+        [childModel.sequelize.col(groupBy), 'key'],
+        [childModel.sequelize.fn(aggregate, childModel.sequelize.col(aggregateField)), 'value'],
+      ],
+      where: parentSequelizeOptions.where,
+      includeIgnoreAttributes: false,
+      include: [{
+        model: childModel,
+        attributes: [labelField],
+        as: associationAs,
+        required: true,
+        where: childSequelizeOptions.where,
+        include: childSequelizeOptions.include || [],
+      }, ...(parentSequelizeOptions.include || [])],
+      subQuery: false,
+      group: groupBy,
+      order: [[childModel.sequelize.literal('value'), 'DESC']],
+      limit,
+      raw: true,
+    });
 
     return {
       value: records.map((data) => ({
