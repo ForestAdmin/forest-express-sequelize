19137e96aa61eb19d21c65c7efaadb11a26fb54c
Arnaud Moncel <arnaud.moncel43@gmail.com>
2021-10-20 10:42:29 +0200

    chore: update types

--- types/index.d.ts
+++ types/index.d.ts
@@ -227,7 +227,7 @@ export interface SmartFieldSearchQuery {
 }
 
 export interface SmartFieldSearcher {
-  (query: SmartFieldSearchQuery, search: string): SmartFieldSearchQuery;
+  (query: SmartFieldSearchQuery, search: string): SmartFieldSearchQuery | Promise<SmartFieldSearchQuery>;
 }
 
 export interface SmartFieldFiltererFilter {
