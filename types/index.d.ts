91cc4c8046635c771df52db5c1d6709aa3d060e4
Justin Martin <36707217+JustinMartinDev@users.noreply.github.com>
2022-09-27 12:04:12 +0200

    Add "isSortable" to SmartFieldOptions interface
    According to doc: https://docs.forestadmin.com/documentation/reference-guide/smart-fields/smart-field-examples/sort-by-smart-field, smart field option should allow to set property `isSortable` as optional boolean

--- types/index.d.ts
+++ types/index.d.ts
@@ -274,6 +274,7 @@ export interface SmartFieldOptions {
   description?: string;
   type: FieldType;
   isFilterable?: boolean;
+  isSortable?: boolean;
   isReadOnly?: boolean;
   isRequired?: boolean;
   reference?: string;
