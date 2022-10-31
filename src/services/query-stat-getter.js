da38ed239b38fcae01b720d271048d436cbcf782
Vincent Molinié <vincent.m@forestadmin.com>
2022-10-31 14:40:02 +0100

    feat(chart): add support for context variables inside SQL query

--- src/services/query-stat-getter.js
+++ src/services/query-stat-getter.js
@@ -1,18 +1,21 @@
 
 function QueryStatGetter(params, opts) {
-  const QUERY_OPTIONS_SELECT = { type: opts.Sequelize.QueryTypes.SELECT };
-
   this.perform = function perform() {
     let rawQuery = params.query.trim();
+    const bind = params.contextVariables || {};
 
 
-    if (params.record_id) {
-      rawQuery = rawQuery.replace(new RegExp('\\?', 'g'), params.record_id);
+    if (params.record_id && !rawQuery.includes('$$recordId')) {
+      rawQuery = rawQuery.replace(/\\?/g, '$$$$recordId');
+      bind.recordId = params.record_id;
     }
 
     // WARNING: Choosing the first connection might generate issues if the model
     //          does not belongs to this database.
-    return Object.values(opts.connections)[0].query(rawQuery, QUERY_OPTIONS_SELECT);
+    return Object.values(opts.connections)[0].query(rawQuery, {
+      type: opts.Sequelize.QueryTypes.SELECT,
+      bind,
+    });
   };
 }
 
